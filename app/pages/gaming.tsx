7bf18b76d43223c311a1b94965b3824df0876576
Thorben <bender.thorben0@gmail.com>
2021-01-23 19:37:43 +0100

    Clean up code. And remove initial stuff from create-next-app

---
+++ app/pages/gaming.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+type Props = {
+
+}
+
+const Gaming = (props: Props) => {
+    return (
+        <div>
+            <p>Gaming</p>
+        </div>
+    )
+}
+
+export default Gaming;
